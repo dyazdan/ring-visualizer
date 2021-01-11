fe48f6b7253ab8d43178ccd1b8d37e3f027b192a
Dennis Orner <code@dyaz.de>
2021-01-11 16:41:05 +0100

    Added changing colors, resizability, GitHub link and more comments, removed unnecessary assignments

--- visualizer.js
+++ visualizer.js
@@ -1,39 +1,43 @@
-console.debug('ring visualizer script loaded');
+console.debug('visualizer script loaded');
 
 class Visualizer {
 
-    #width;     // The width of the canvas in px
-    #height;    // The height of the canvas in px
-    #size;      // The shortest side of the visualization canvas in px
-    #color      // The visualisation color in rgb();
+    #size = 150;                    // The shortest side of the visualization canvas in px, defaults to 150
+    #color = 'rgb(0, 191, 255)';    // The visualization color in rgb(), defaults to a nice blue
 
     #isReady = false;       // True after successful initialization
     #isVisualizing = false; // True if the visualizer is currently animating
 
-    #mediaElement;  // The audio/video DOM element the audio context relates to
+    #mediaElement;  // The audio/video DOM element the audio context relates to, identified by 'player'
     #audioContext;  // The audio context
     #audioSource;   // The audio source created from the media element
 
-    #analyser;
-    #analyserTimeDomainData;
-    #analyserFrequencyData;
+    #analyser;                  // The audio analyser node
+    #analyserTimeDomainData;    // The array holding the analyser time domain data (typically ~127 when idle)
+    #analyserFrequencyData;     // The array holding the analyser frequency data (in the 0-255 range)
 
-    #canvas;
-    #canvasContext;
-    #domElement;
+    #canvas;        // The HTML canvas element the visualizer draws on
+    #canvasContext; // The canvas' 2D context
+    #domElement;    // The DOM element that is replaced by the visualizer, identified by 'visualizer'
 
+    // Fire me up!
     constructor() {
         console.debug(`visualizer constructed`);
     }
 
+    // Initializes the visualizer
     init() {
+
+        // Locate the HTML element representing an audio source (i.e. player)
         this.#mediaElement = document.getElementById('player');
         if (!this.#mediaElement) throw 'media element not found';
 
+        // Get the audio context
         this.#audioContext = new (window.AudioContext || window.webkitAudioContext)()
         this.#audioSource = this.#audioContext.createMediaElementSource(this.#mediaElement);
         if (!(this.#audioSource && this.#audioContext)) throw `couldn't set up audio source or context`;
 
+        // Create and configure the analyzer
         this.#analyser = this.#audioContext.createAnalyser();
         this.#analyser.fftSize = 64;
         this.#analyser.minDecibels = -128;
@@ -47,26 +51,28 @@ class Visualizer {
             throw `couldn't set up analyzer`;
         }
 
+        // Create the canvas
         this.#canvas = document.createElement('canvas');
-
         this.#canvas.id = 'visualizer-canvas';
         this.#canvas.width = this.#canvas.height = this.#size;
         this.#canvasContext = this.#canvas.getContext("2d");
-
         if (!(this.#canvas && this.#canvasContext)) {
             throw `couldn't set up canvas or canvas context`;
         }
 
+        // Find the HTML DOM element to replace by the visualizer
         this.#domElement = document.getElementById('visualizer');
+        if (!this.#domElement) {
+            throw `couldn't set up DOM element`;
+        }
 
+        // Create the <div> container holding the canvas
         const container = document.createElement('div');
-        container.setAttribute('width', `${this.#size}px`);
-        container.setAttribute('height', `${this.#size}px`);
         container.append(this.#canvas);
         this.#domElement.replaceWith(container);
         container.id = 'visualizer';
 
-        // Sneak into the styles
+        // Sneak into the computed CSS-styles; This is necessary to correctly size the canvas render area
         let domElementStyle = getComputedStyle(this.#canvas);
         const width = domElementStyle.getPropertyValue('width').replace('px', '');
         const height = domElementStyle.getPropertyValue('height').replace('px', '');
@@ -75,19 +81,15 @@ class Visualizer {
         this.#canvas.height = height;
         this.#size = width > height ? height : width;
         this.#color = color;
+        console.debug(`visualizer canvas derived ${this.#canvas.width}x${this.#canvas.height} from styles`);
 
-        console.log(this.#canvas.width);
-
-        if (!this.#domElement) {
-            throw `couldn't set up DOM element`;
-        }
-
-        // If we're all done and nothing threw an error
+        // If we're all done and nothing threw an error, let's go!
         this.#isReady = true;
-
-        console.debug(`visualizer initialized with size ${this.#size}`);
+        console.debug(`visualizer initialized:`);
+        console.debug(this);
     }
 
+    // Starts the visualization and animation loop
     start() {
         if (this.#isReady === true) {
             this.#isVisualizing = true;
@@ -98,6 +100,7 @@ class Visualizer {
         }
     }
 
+    // Stops the visualization
     stop() {
         if (this.#isVisualizing === true) {
             this.#isVisualizing = false;
@@ -107,6 +110,7 @@ class Visualizer {
         }
     }
 
+    // The main animation method containing the animation loop
     animate() {
         console.debug('visualizer starting animation');
         const visualizer = this;
@@ -116,9 +120,19 @@ class Visualizer {
         const canvas = visualizer.#canvas;
         const canvasContext = visualizer.#canvasContext;
 
+        // Realising the animation loop by defining a frame-drawing method, recursively requesting animation frames
         let frame = function () {
 
+            // Not necessarily cool for performance, but fancy: Grab the current style info from the DOM
+            let domElementStyle = getComputedStyle(visualizer.#canvas);
+            visualizer.#color = domElementStyle.getPropertyValue('color');
+            canvas.width = domElementStyle.getPropertyValue('width').replace('px', '');
+            canvas.height = domElementStyle.getPropertyValue('height').replace('px', '');
+
+            // Check whether the visualization should be running
             if (visualizer.#isVisualizing === true) {
+
+                // Request the next animation frame
                 requestAnimationFrame(frame);
 
                 // Get the time domain and frequency data from the analyzer
@@ -135,8 +149,10 @@ class Visualizer {
                 for (let i = 0; i < analyserTimeDomainData.length; i++) {
                     let radius = analyserTimeDomainData[i] * (maxRadius / 255);
 
+                    // Calculate the opacity
                     let opacity = (1 / 255) * Math.abs(analyserTimeDomainData[i] - 127) * 2;
 
+                    // Draw the current ring
                     canvasContext.beginPath();
                     canvasContext.arc(canvas.width / 2, canvas.height / 2, radius, 0, 2 * Math.PI);
                     canvasContext.strokeStyle = `${visualizer.#color.replace('rgb', 'rgba').replace(')', ','+ opacity + ')')}`;
@@ -188,8 +204,11 @@ class Visualizer {
 
 }
 
+// Create a new visualizer instance
 let visualizer = new Visualizer();
 
+// Calls the init method from within this file
+// Can be removed and called from outside (e.g. after restarting the audio context)
 try {
     visualizer.init();
 } catch (e) {
